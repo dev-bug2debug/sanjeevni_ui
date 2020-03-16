a6c21357abb2799fbd214dbc5eb70988df8937fd
hitenjain88 <hitenjain88@gmail.com>
2020-03-16 23:16:03 +0530

    Login Screen added

---
+++ lab_webapp/lib/screens/LoginScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return LoginScreenState();
+  }
+}
+
+class LoginScreenState extends State<LoginScreen>{
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
