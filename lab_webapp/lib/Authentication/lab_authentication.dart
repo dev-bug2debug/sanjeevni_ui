8dd4bb8255d3fc9828ce2c390b27b6c1a1c93921
rana <ranaatul9211@gmail.com>
2020-03-20 02:29:28 +0530

    Merge remote-tracking branch 'origin/alpha' into alpha
    # Conflicts:
    #	lab_webapp/lib/screens/LoginScreen.dart

---
+++ lab_webapp/lib/Authentication/lab_authentication.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+class Authentication{
+
+  labSignUp(String email, String password, String confirmpass) async {
+    Map data={
+    'mail' : email,
+    'pass' : password,
+    'cpass' : confirmpass
+    };
+
+    // Api calling ......note this api is hosted locally
+    final response = await http.post('http://127.0.0.1:8000/labSignUp/',body: data);
+    if (response.statusCode == 200) {
+      debugPrint('succesfull');
+    } else {
+      print(response.statusCode);
+    }
+  }
+  labSignIn(String email,String password){
+    
+  }
+
+}
