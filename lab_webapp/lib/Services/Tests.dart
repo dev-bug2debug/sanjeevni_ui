4b720967eff71914c22211b2137621d5cf5ee2bb
31shavy@gmail.com <31shavy@gmail.com>
2020-04-09 18:40:11 +0530

    method added

--- lab_webapp/lib/Services/Tests.dart
+++ lab_webapp/lib/Services/Tests.dart
@@ -8,7 +8,18 @@ class test{
 
   // this method return a list of tests which is added by kartikey in the firebase
   Future  getData() async{
-    http.Response response= await http.get("http://127.0.0.1:8000/testdata/");
+
+   // ?  Response response = await post(url, headers: headers, body: json)
+    // ! json data = {"type":"get", "test":"*"} for all
+    // ! json data = {"type":"get", "test":"*Name"} for all keys
+    // ! json data = {"type":"get", "test":"__TEST__NAME__"}
+
+    var json_data = {"type":"get", "test":"*Name"};
+    http.Response response= await http.post("http://127.0.0.1:8000/api/tests", body:json_data);
+
+    // * http.Response response= await http.get("http://127.0.0.1:8000/api/tests");
+
+
     data=json.decode(response.body);
 
     // yha jo set state and initstate hai ye us widget me lagana jaha se call karega is 
